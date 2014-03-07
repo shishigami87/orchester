d5ed8565b64f73a2bb183f1f0acf740ad87d2b0b
shishigami87 <shishigami@hotmail.de>
2014-03-07 11:53:59 +0100

    Added create

--- src/main/java/eu/shishigami/orchester/web/controller/AdresseController.java
+++ src/main/java/eu/shishigami/orchester/web/controller/AdresseController.java
@@ -4,7 +4,9 @@ import eu.shishigami.orchester.domain.entity.AdresseEntity;
 import eu.shishigami.orchester.domain.service.AdresseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,21 +27,28 @@ public class AdresseController {
         return adresseService.findAll();
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody AdresseEntity saveAdresse(@RequestBody AdresseEntity adresseEntity) {
+        System.out.println(adresseEntity.toString());
+        return adresseService.save(adresseEntity);
+    }
+
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public @ResponseBody AdresseEntity getAdresseById(@PathVariable Long id) {
         return adresseService.findOne(id);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<AdresseEntity> deleteAdresse(@PathVariable Long id) {
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteAdresse(@PathVariable Long id) {
         AdresseEntity adresseEntity = adresseService.findOne(id);
         adresseService.delete(adresseEntity);
+    }
 
-        if (adresseService.findOne(id) == null) {
-            return new ResponseEntity<AdresseEntity>(adresseEntity, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<AdresseEntity>(adresseEntity, HttpStatus.UNPROCESSABLE_ENTITY);
-        }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleBadRequest(HttpMessageNotReadableException e) {
+        e.printStackTrace();
     }
 
 }
--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -3,10 +3,20 @@ var app = angular.module('app', [
     'ngResource'
 ]);
 
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'partials/home.html',
         controller: 'HomeController'
+    });
+
+    $routeProvider.when('/adresse/', {
+        templateUrl: 'partials/adresse/overview.html',
+        controller: 'AdresseOverviewController'
+    });
+
+    $routeProvider.when('/adresse/create/', {
+        templateUrl: 'partials/adresse/create.html',
+        controller: 'AdresseCreateController'
     });
 
     $routeProvider.otherwise({
@@ -14,13 +24,40 @@ app.config(function($routeProvider) {
     });
 });
 
-app.controller('HomeController', function($scope, $route, AdresseFactory) {
+app.controller('HomeController', function ($scope, $route) {
     $scope.title = "Home";
-    $scope.adressen = AdresseFactory.query();
 });
 
-app.factory('AdresseFactory', function($resource) {
+app.controller('AdresseOverviewController', function ($scope, $route, $location, AdresseFactory) {
+    $scope.title = "Adressen";
+    $scope.adressen = AdresseFactory.query();
+
+    $scope.handleCreate = function () {
+        $location.path('/adresse/create/')
+    };
+});
+
+app.controller('AdresseCreateController', function ($scope, $route, $location, AdresseFactory) {
+    $scope.title = "Adresse erstellen";
+
+    $scope.handleSave = function () {
+        var newAdresse = new AdresseFactory();
+        newAdresse.plz = $scope.plz;
+        newAdresse.ort = $scope.ort;
+        newAdresse.strasse = $scope.strasse;
+        newAdresse.hausnummer = $scope.hausnummer;
+        newAdresse.$save();
+        $location.path('/adresse/');
+    };
+
+    $scope.handleBack = function () {
+        $location.path('/adresse/');
+    };
+});
+
+
+app.factory('AdresseFactory', function ($resource) {
     return $resource('/api/adresse/:id', {
-        id : '@id'
+        id: '@id'
     });
 });
